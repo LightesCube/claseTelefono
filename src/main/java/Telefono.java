57efa140116109697b2ff7fb0c02bab62390f99f
LightesCube <diegomarillanblooop@gmail.com>
2023-04-19 10:14:45 -0400

    Initial commit

---
+++ src/main/java/Telefono.java
@@ -0,0 +1,22 @@
+public class Telefono {
+    //Atributos
+    String modelo;
+    String marca;
+    String color;
+
+    public Telefono(String modelo, String marca, String color){
+        this.modelo = modelo;
+        this.marca = marca;
+        this.color = color;
+    }
+
+    public void llamar() {
+
+    }
+
+    public void reproducir() {
+
+    }
+
+    public void
+}
---
+++ src/main/java/claseObjeto.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class claseObjeto {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Crear telefono");
+        Telefono telefono1 =
+    }
+}
