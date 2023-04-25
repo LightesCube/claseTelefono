540d47360738b438e4fd043cf1b64bfe83b3ba3b
LightesCube <diegomarillanblooop@gmail.com>
2023-04-24 20:45:47 -0400

    Tarea terminada

--- src/main/java/Telefono.java
+++ src/main/java/Telefono.java
@@ -2,21 +2,19 @@ public class Telefono {
     //Atributos
     String modelo;
     String marca;
-    String color;
 
-    public Telefono(String modelo, String marca, String color){
+    public Telefono(String modelo, String marca) {
         this.modelo = modelo;
         this.marca = marca;
-        this.color = color;
     }
 
     public void llamar() {
-
+        System.out.println(modelo + "!!");
     }
 
     public void reproducir() {
-
+        System.out.println("el telefono es un " + modelo + " y su marca es " + marca
+        );
     }
-
-    public void
 }
+
--- src/main/java/claseObjeto.java
+++ src/main/java/claseObjeto.java
@@ -4,6 +4,14 @@ public class claseObjeto {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Crear telefono");
-        Telefono telefono1 =
+        System.out.println("Ingrese modelo del telefono");
+        String modelo = teclado.next();
+        System.out.println("Ingrese marca del telefono");
+        String marca = teclado.next();
+    Telefono telefono1 = new Telefono (modelo,marca);
+
+
+    telefono1.llamar();
+    telefono1.reproducir();
     }
 }
